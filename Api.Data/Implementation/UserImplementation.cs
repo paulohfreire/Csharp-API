e7c275a57580c129858c3e78c1967314b68ba676
Paulo <paulofreire@frwk.com.br>
2021-11-10 15:58:46 -0300

    User implementation added

---
+++ Api.Data/Implementation/UserImplementation.cs
@@ -0,0 +1,27 @@
+﻿using Api.Data.Context;
+using Api.Data.Repository;
+using Api.Domain.Repository;
+using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Data.Implementation
+{
+    public class UserImplementation : BaseRepository<UserEntity>, IUserRepository
+    {
+        private DbSet<UserEntity> _dataset;
+
+        public UserImplementation(MyContext context) : base(context)
+        {
+            _dataset = context.Set<UserEntity>(); 
+        }
+
+        public async Task<UserEntity> FindByLogin(string email)
+        {
+            return await _dataset.FirstOrDefaultAsync(u => u.Email.Equals(email));
+        }
+    }
+}
---
+++ Api.Domain/Interfaces/Services/User/ILoginService.cs
@@ -0,0 +1,13 @@
+﻿using Api.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Domain.Interfaces.Services.User
+{
+    public interface ILoginService
+    {
+        Task<object> FindByLogin(UserEntity user);
+    }
+}
