fd5e46be48902497414f54fa00e78d5dfa772e25
Paulo <paulofreire@frwk.com.br>
2021-11-10 14:19:32 -0300

    add swagger

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,16 +1,9 @@
 using Api.CrossCutting.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApplication1
 {
@@ -29,6 +22,7 @@ namespace WebApplication1
             ConfigureService.ConfigureDependeciesService(services);
             ConfigureRepository.ConfigureDependeciesRepository(services);
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +34,13 @@ namespace WebApplication1
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Curso de API");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
