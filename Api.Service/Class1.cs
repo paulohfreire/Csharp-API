57847484bcb69e7f8b08733571b88c2694b67a50
Paulo <paulofreire@frwk.com.br>
2021-11-11 11:19:36 -0300

    remove unusual using elements

--- Api.Service/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Api.Service
-{
-    public class Class1
-    {
-    }
-}
--- WebApplication1/Controllers/UsersController.cs
+++ WebApplication1/Controllers/UsersController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
     [Route("api/[controller]")]
     [ApiController]
         public class UsersController : ControllerBase
-    {
+        {
 
         private IUserService _service;
 
