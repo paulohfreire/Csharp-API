ed8798d83bfa81f98c41cc9e8c8afcd3abe91f22
Paulo <paulofreire@frwk.com.br>
2021-11-16 17:02:26 -0300

    add first service tests

---
+++ Api.Service.Test/Usuario/UsuariosTestes.cs
@@ -0,0 +1,79 @@
+﻿using Api.Domain.Dtos.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Service.Test.Usuario
+{
+    public class UsuariosTestes
+    {
+        public static string NomeUsuario { get; set; }
+        public static string EmailUsuario { get; set; }
+        public static string NomeUsuarioAlterado { get; set; }
+        public static string EmailUsuarioAlterado { get; set; }
+        public static Guid IdUsuario { get; set; }
+
+        public List<UserDto> listaUserDto = new List<UserDto>();
+        public UserDto userDto;
+        public UserDtoCreate userDtoCreate;
+        public UserDtoCreateResult userDtoCreateResult;
+        public UserDtoUpdate userDtoUpdate;
+        public UserDtoUpdateResult userDtoUpdateResult;
+
+        public UsuariosTestes()
+        {
+            IdUsuario = Guid.NewGuid();
+            NomeUsuario = Faker.Name.FullName();
+            EmailUsuario = Faker.Internet.Email();
+            NomeUsuarioAlterado = Faker.Name.FullName();
+            EmailUsuarioAlterado = Faker.Internet.Email();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var dto = new UserDto()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = Faker.Name.FullName(),
+                    Email = Faker.Internet.Email()
+                };
+                listaUserDto.Add(dto);
+            }
+
+            userDto = new UserDto
+            {
+                Id = IdUsuario,
+                Name = NomeUsuario,
+                Email = EmailUsuario
+            };
+
+            userDtoCreate = new UserDtoCreate
+            {
+                Name = NomeUsuario,
+                Email = EmailUsuario
+            };
+
+            userDtoCreateResult = new UserDtoCreateResult
+            {
+                Id = IdUsuario,
+                Name = NomeUsuario,
+                Email = EmailUsuario,
+                CreateAt = DateTime.UtcNow
+            };
+
+            userDtoUpdate = new UserDtoUpdate
+            {
+                Name = NomeUsuario,
+                Email = EmailUsuario
+            };
+
+            userDtoUpdateResult = new UserDtoUpdateResult
+            {
+                Id = IdUsuario,
+                Name = NomeUsuario,
+                Email = EmailUsuario,
+                UpdateAt = DateTime.UtcNow
+            };
+
+        }
+    }
+}
